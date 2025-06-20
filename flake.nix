69ca820f328df7514b9e5e8d6604c57ea7b5d1df
Juan A. <adrephos@gmail.com>
2025-06-20 09:52:38 -0500

    feat: update

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
             src = pkgs.fetchurl {
               curlOpts = "--user-agent 'Mozilla/5.0'";
               url = "https://boosteroid.com/linux/installer/boosteroid-install-x64.deb";
-              hash = "sha256-R9pj8gb9cxhCorXr9CQnRf0LxpB0b3tZCzDmJgNJC48=";
+              hash = "sha256-tXQetaTFAfjZx9Hnjwa3GzzBD++pgNpKKPB1b2OsBjs=";
             };
 
             nativeBuildInputs = with pkgs; [
